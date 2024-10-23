e8a899e727cef6a19024ba4e4646258c782caf24
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2024-10-23 23:02:35 +0700

    show message error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -9,7 +9,7 @@ export default function Error(){
         <div className="container mx-auto">
             <div className="flex flex-col w-full h-screen justify-center items-center space-y-4">
                 <h1 className="text-6xl">OOPS!</h1>
-                <p>{isRouteError ? `${error.status} - ${error.statusText}` : 'Something went wrong'}</p>
+                <p>{isRouteError ? `${error.status} - ${error.data.message || error.statusText}` : 'Something went wrong'}</p>
             </div>
         </div>
     )
